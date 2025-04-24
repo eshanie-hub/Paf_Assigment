d74bb0736f4fbdbbb2a159f19dc7c4271f610d43
Binuki Mihara <133636115+BinukiMihara@users.noreply.github.com>
2025-04-24 16:46:12 +0530

    UI created for the My learning progress table

---
+++ frontend/src/pages/learning_progress/Learning_progress_table_create.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { Modal, Button, Form } from 'react-bootstrap';
+import axios from 'axios';
+
+const LearningProgressTableCreate = ({ show, handleClose, refreshData }) => {
+
+
+    return (
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Add New Course</Modal.Title>
+            </Modal.Header>
+            <Form onSubmit={handleSubmit}>
+                <Modal.Body>
+                    <Form.Group>
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="title"
+                            value={formData.title}
+                            onChange={handleChange}
+                            required
+                        />
+                    </Form.Group>
+                    <Form.Group className="mt-2">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="description"
+                            value={formData.description}
+                            onChange={handleChange}
+                            required
+                        />
+                    </Form.Group>
+                    <Form.Group className="mt-2">
+                        <Form.Label>Progress</Form.Label>
+                        <Form.Control
+                            type="number"
+                            name="progress"
+                            value={formData.progress}
+                            onChange={handleChange}
+                            required
+                        />
+                    </Form.Group>
+                    <Form.Group className="mt-2">
+                        <Form.Label>Deadline</Form.Label>
+                        <Form.Control
+                            type="date"
+                            name="deadline"
+                            value={formData.deadline}
+                            onChange={handleChange}
+                            required
+                            min={new Date().toLocaleDateString('en-CA')}    //validation added- user can select today's date or future dates only
+                        />
+                    </Form.Group>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>Cancel</Button>
+                    <Button type="submit" variant="primary">Add Course</Button>
+                </Modal.Footer>
+            </Form>
+        </Modal>
+    );
+};
+
+export default LearningProgressTableCreate;
