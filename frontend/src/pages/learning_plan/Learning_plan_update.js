8a322ed55c3821d0367b796ada35a6c77d04eb05
eshanie <eshaniechathurika@gmail.com>
2025-04-24 16:43:58 +0530

    learning claendar ui

--- frontend/src/pages/learning_plan/Learning_plan_update.js
+++ frontend/src/pages/learning_plan/Learning_plan_update.js
@@ -3,6 +3,100 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
 
 export const LearningPlanUpdate = () => {
+    const params = useParams();
+  const navigate = useNavigate();
+    const [state, setState] = useState({
+        title: "",
+        description: "",
+        date: "",
+        startTime: "",
+        endTime: ""
+      })
+
+    
+      const [errors, setErrors] = useState({});
+      const [submitting, setSubmitting] = useState(false);
+
+      const validateValues = (inputValues) => {
+        let errors = {};
+        if (inputValues.title.length < 1) {
+          errors.title = "title can't be null";
+        }
+
+        if (inputValues.description.length < 1) {
+          errors.description = "description can't be null";
+        }
+
+      
+        if (inputValues.date.length < 1) {
+          errors.date = "date can't be null";
+        }
+        
+        
+        //time can't be null
+        if (inputValues.startTime < 1) {
+          errors.startTime = "time can't be null";
+        }
+
+        //time can't be null
+        if (inputValues.endTime < 1) {
+          errors.endTime = "time can't be null";
+        }
+
+        return errors;
+      };
+
+      const handleChange = (e) =>{
+        setState({ ...state, [e.target.name]: e.target.value });
+        setErrors(validateValues(state));
+      }
+      
+      const handleSubmit = (event) =>{
+        event.preventDefault();
+        setErrors(validateValues(state));
+        setSubmitting(true);
+
+        if(Object.keys(errors).length === 0 && submitting){
+          const 
+        {
+            title, 
+            description, 
+            date,
+            startTime,
+            endTime
+        } = state;
+    
+        const data = {
+          title: title,
+          description: description,
+          date: date,
+          startTime: startTime,
+          endTime: endTime
+        }
+        console.log(data);
+
+        axios.put(`http://localhost:8080/api/learningPlan/${params.id}`, data)
+        .then((res) => {
+          alert("Data submited successfully");
+          navigate(-1);
+        })
+      }
+      }
+      
+      useEffect(() => {
+        axios.get(`http://localhost:8080/api/learningPlan/${params.id}`).then((res) => {
+          if(res.data){
+            setState({
+              title: res.data.title,
+              description: res.data.description,
+              date: res.data.date,
+              startTime: res.data.startTime,
+              endTime: res.data.endTime
+            })
+            
+          }
+        })
+      },[params.id]);
     return (
         <div className='p-5'>
           <h3 className='pb-5 text-center'>Learning Plan Edit</h3>
