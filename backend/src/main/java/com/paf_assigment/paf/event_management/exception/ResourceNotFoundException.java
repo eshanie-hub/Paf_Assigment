f81edb9361d0f1f3d93425becd024ea403a8157e
thiyaraPG <thiyaragunawardena@gmail.com>
2025-05-06 23:55:26 +0530

    Exceptions created

---
+++ backend/src/main/java/com/paf_assigment/paf/event_management/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.paf_assigment.paf.event_management.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
